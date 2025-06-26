97cd13c92486e0fac12e05bb6b80caff06446979
NiiAmui <samniiamui@gmail.com>
2025-06-26 14:59:12 +0000

    update the store config page with the payment config table

--- src/app/private/settings/store-configuration/page.tsx
+++ src/app/private/settings/store-configuration/page.tsx
@@ -9,6 +9,10 @@ import { InputFormField, SwitchFormField } from '@/customComponents/FormFields'
 import ButtonLoading from '@/customComponents/Button'
 import { ghanaPhoneRegex } from '@/lib/globalConstants'
 import { useForm } from 'react-hook-form'
+import { ColumnDef } from '@tanstack/react-table'
+import DataTable from '@/customComponents/datatable'
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover'
+import IconifyIcon from '@/customComponents/IconifyIcon'
 
 
 const productConfigurationSchema = z.object({
@@ -17,6 +21,99 @@ const productConfigurationSchema = z.object({
   showTaxInclusivePrices: z.boolean(),
   showDiscounts: z.boolean().optional(),
 })
+
+
+const paymentMethodData = [
+  {
+    type: 'Mobile Money',
+    provider: 'MTN',
+    accountNumber: '0541234567',
+    status: 'Active',
+  },
+  {
+    type: 'Mobile Money',
+    provider: 'Telecel',
+    accountNumber: '0206789063',
+    status: 'Active',
+  },
+  {
+    type: 'Bank',
+    provider: 'Access Bank',
+    accountNumber: '10344556789123',
+    status: 'Active',
+  },
+  {
+    type: 'Card Payment',
+    provider: 'Master Card',
+    accountNumber: '1234-5678-9012-3456',
+    status: 'Active',
+  },
+  {
+    type: 'Gateway',
+    provider: 'Paystack',
+    accountNumber: 'paystack-123456',
+    status: 'Not Active',
+  },
+]
+
+function PopoverDemo() {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <IconifyIcon icon='solar:menu-dots-bold' />
+      </PopoverTrigger>
+      <PopoverContent className="w-40 px-2">
+        <div className="grid gap-3">
+          <div className="">
+            {/* <p className="leading-none font-medium text-sm">Actions</p> */}
+
+          </div>
+          <div className="grid gap-2 text-xs text-darkGrey">
+            <div className="grid p-2 hover:bg-gray-50">
+              Edit
+            </div>
+            <div className="grid p-2 hover:bg-gray-50">
+              Disable
+            </div>
+            <div className="grid p-2 hover:bg-gray-50">
+              Delete
+            </div>
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+const paymentMethodColumns: ColumnDef<any>[] = [
+  {
+    header: "Type",
+    id: "type",
+    accessorKey: "type",
+  },
+  {
+    header: "Provider",
+    id: "provider",
+    accessorKey: "provider",
+  },
+  {
+    header: "Account Number",
+    id: "accountNumber",
+    accessorKey: "accountNumber",
+  },
+  {
+    header: "Status",
+    id: "status",
+    accessorKey: "status",
+  },
+  {
+    header: "Actions",
+    id: "actions",
+    cell: ({ row }: any) => (
+      <PopoverDemo />
+    ),
+  },
+]
 
 const page = () => {
   const storeConfigForm = useForm<z.infer<typeof productConfigurationSchema>>({
@@ -65,9 +162,20 @@ const page = () => {
 
       </section>
 
-      <div className="paymentMethodTable mt-10">
+      <section className="paymentMethodTable mt-10">
+        <DataTable tableInformationContent={<div className='pb-5 flex justify-between'>
+          {/* left side */}
+          <div className="leftSide">
+            <p className='text-xl font-semibold'>Payment Method</p>
+            <p className='mt-1 text-mediumGrey text-sm'>Set up your preferred payment options to receive money from customers</p>
+          </div>
+          {/* right side */}
+          <div className="rightSide">
+            <ButtonLoading title='Add Payment Method' leftIcon='formkit:add' className='cursor-pointer w-fit' onClick={() => { console.log("Add Payment Method Clicked") }} />
+          </div>
 
-      </div>
+        </div>} columns={paymentMethodColumns} data={paymentMethodData} totalPages={1} />
+      </section>
     </div>
   )
 }
