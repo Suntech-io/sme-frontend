8d1dc9da338085d646807e7da75891b897b45c51
NiiAmui <samniiamui@gmail.com>
2025-09-06 02:02:43 +0000

    update stock batches

--- src/app/private/catalog/stocks/details/[stockId]/partials/AddStockPackageSlider.tsx
+++ src/app/private/catalog/stocks/details/[stockId]/partials/AddStockPackageSlider.tsx
@@ -63,7 +63,34 @@ const AddStockPackageSlider = ({ open, onOpenChange }: AddBatchSliderProps) => {
                     </Form>
 
                     {/* card on selected unit size */}
-                    
+                    <div className="selectedUnitSizeContainer border bg-gray-100 p-8 rounded-xl my-10">
+
+                        {/* card title */}
+                        <p className="cardTitle text-primary mb-4">
+                            Selected Unit size
+                        </p>
+
+                        {/* unit card */}
+                        <div className='UnitSizeCard bg-white hover:scale-[100.2%] rounded-lg p-4 shadow-sm hover:shadow-md transition-all'>
+                            {/* header */}
+                            <div className="header flex items-center justify-between">
+                                {/* title */}
+                                <p className="unitTitle font-thin text-gray-500 text-lg">Small</p>
+
+                            </div>
+                            {/* value */}
+                            <div className="value mt-4 flex items-center space-x-2">
+                                <p className='text-gray-600 text-3xl font-semibold'>5</p>
+                                <p className='text-[11px] bg-primary/10 px-4 py-0.5 rounded-full'>Litres</p>
+                            </div>
+
+                            {/* packages unit size has been used in */}
+                            <div className="packagesUsedIn mt-4">
+                                <p className='text-sm text-mediumGrey italic font-light'>Used in 3 Packages</p>
+                            </div>
+
+                        </div>
+                    </div>
                 </div>
 
                 <SheetFooter>
